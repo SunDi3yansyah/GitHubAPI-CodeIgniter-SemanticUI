c5ec68ae0f92cdd983917a6351c98adddac1cd9e
SunDi3yansyah <sundi3yansyah@gmail.com>
2016-02-11 02:49:18 +0700

    test push heroko

--- application/config/config_custom.php
+++ application/config/config_custom.php
@@ -13,7 +13,7 @@
 |--------------------------------------------------------------------------
 */
 
-$config['token'] = 'SHA1';
+$config['token'] = '294cd71200620180d041f2d1ebb4925b05604682';
 
 /*
 |--------------------------------------------------------------------------
@@ -21,16 +21,13 @@ $config['token'] = 'SHA1';
 |--------------------------------------------------------------------------
 */
 
-$config['website_name'] = 'Your site name';
-$config['about'] = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-
-// Replace the value if you using typed.js
-$config['about_typed'] = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-
-$config['title_default'] = 'Write your title here';
-$config['description'] = 'Write your description here';
-$config['keywords'] = 'keywords1, keywords2, keywords3, etc...';
-$config['author'] = 'What your name?';
+$config['website_name'] = 'Cahyadi Triyansyah';
+$config['about'] = 'My name is Cahyadi Triyansyah (SunDi3yansyah) from Yogyakarta, Indonesia. I am part of the developer in the server such as DNS Server, Web Server, Mail Server, and so forth. While the website using CodeIgniter to prioritize current and most are native PHP. To the Client Side I thrive in almost all layers are CSS, HTML, JavaScript including jQuery and various frontend like twitter bootstrap framework. In addition I often indulge in github to learn from it as code for my home.';
+$config['about_typed'] = '"Hello World...!^1500", "My name is Cahyadi Triyansyah (SunDi3yansyah) from Yogyakarta, \nIndonesia.^1000 I am part of the developer in the server such as DNS \nServer,^800 Web Server,^600 Mail Server, and so forth.^1000 While the website using CodeIgniter to prioritize current and most are native PHP.^1000 To the \nClient Side I thrive in almost all layers are CSS,^500 HTML,^500 JavaScript \nincluding jQuery^800 and various frontend like twitter bootstrap framework.^1000 In addition I often indulge in github to learn from it as code for \nmy home."';
+$config['title_default'] = 'Showcase | SunDi3yansyah';
+$config['description'] = 'Showcase | SunDi3yansyah';
+$config['keywords'] = 'Showcase | SunDi3yansyah';
+$config['author'] = 'Cahyadi Triyansyah';
 
 /*
 |--------------------------------------------------------------------------
@@ -39,19 +36,19 @@ $config['author'] = 'What your name?';
 */
 
 $config['email'] = TRUE;
-$config['email_address'] = 'your@email.tld';
+$config['email_address'] = 'sundi3yansyah@gmail.com';
 
 $config['github'] = TRUE;
-$config['github_username'] = 'your-username';
+$config['github_username'] = 'SunDi3yansyah';
 
 $config['twitter'] = TRUE;
-$config['twitter_username'] = 'your-username';
+$config['twitter_username'] = 'SunDi3yansyah';
 
 $config['google_plus'] = TRUE;
-$config['google_plus_username'] = 'your-username';
+$config['google_plus_username'] = 'SunDi3yansyahLibrary';
 
 $config['linkedin'] = TRUE;
-$config['linkedin_username'] = 'your-username';
+$config['linkedin_username'] = 'sundi3yansyah';
 
 $config['facebook'] = TRUE;
-$config['facebook_username'] = 'your-username';
+$config['facebook_username'] = 'adiebiazajah';
